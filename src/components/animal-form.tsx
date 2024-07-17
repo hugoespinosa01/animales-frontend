3f6418963b50c24e89b7397e03bf37bbe57d0046
hespinoza <hespinoza@eikon.com.ec>
2024-07-16 23:43:40 -0500

    Se actualiza esquema de Animales

--- src/components/animal-form.tsx
+++ src/components/animal-form.tsx
@@ -8,8 +8,8 @@ export default function Form() {
 
   const animalSchema = z.object({
     nombre: z.string({ required_error: "Nombre es requerido" }).min(1, { message: "El nombre debe tener al menos un caracter" }),
-    numeroPatas: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "Al menos debe el animal debe tener 0 patas" }),
-    alturaMaxima: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "La altura debe ser mayor que 0" })
+    numero_patas: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "Al menos debe el animal debe tener 0 patas" }),
+    altura_maxima: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "La altura debe ser mayor que 0" })
   })
 
   const saveData = (data: z.infer<typeof animalSchema>) => {
