3f05c28f50bed1f3ac32c0f95302728ed3f6f199
hespinoza <hespinoza@eikon.com.ec>
2024-07-16 21:47:11 -0500

    Form de animales

--- src/components/animal-form.tsx
+++ src/components/animal-form.tsx
@@ -1,7 +1,27 @@
+'use client';
 import React from 'react'
+import { z } from "zod";
+import AutoForm from '@/auto-form';
+import { AutoFormSubmit } from '@/auto-form';
+import { Button } from './ui/button';
+
 
 export default function Form() {
+
+  const animalSchema = z.object({
+    nombre: z.string({ required_error: "Nombre es requerido" }).min(1, { message: "El nombre debe tener al menos un caracter" }),
+    numeroPatas: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "Al menos debe el animal debe tener 0 patas" }),
+    alturaMaxima: z.coerce.number({ invalid_type_error: "Debe ser un número" }).gt(0, { message: "La altura debe ser mayor que 0" })
+  })
+
+  const saveData = (data: z.infer<typeof animalSchema>) => {
+    console.log(data);
+  }
+
   return (
-    <div>Form</div>
+    <AutoForm formSchema={animalSchema} onSubmit={saveData}
+    >
+      <AutoFormSubmit />
+    </AutoForm>
   )
 }
